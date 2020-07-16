a8942697ed70e1252733eb51b1dfea198338f2cc
leonid <leonid_demidov.02@mail.ru>
2020-07-16 14:10:11 +0300

    Commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$('.recall__slider').slick({
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    speed: 1500,
+    arrows: true,
+    prevArrow: '<button class="prev"><div class="prev_top"></div><div class="prev_bottom"></div></button>',
+    nextArrow: '<button class="next"><div class="next_top"></div><div class="next_bottom"></div></button>',
+});
